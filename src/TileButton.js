4ed756099a84459317ce4451ef43b8421955f0d6
warham1 <yellow9697@hanyang.ac.kr>
2019-07-10 05:24:10 +0900

    merge graphql code to View

--- src/TileButton.js
+++ src/TileButton.js
@@ -1,15 +1,5 @@
-import React, { Component} from "react";
-import {gql} from "apollo-boost";
-import { Mutation } from "react-apollo";
+import React from "react";
 import "./TileButton.css"
-
-const update_Tile = gql`
-    mutation UpdateTile($tileId: Int!, $tileColor: Int!) {
-        updateTile(tileId: $tileId, tileColor: $tileColor) {
-            val
-        }
-    }
-`;
 
 const getColor = num => {
     switch(num) {
@@ -25,52 +15,17 @@ const getColor = num => {
     }
 }
 
-
-class TileButton extends Component {
+const TileButton = ({tileIdProp, tileColorProp, updateTile}) => 
+{
+    console.log(tileIdProp);
     
-    constructor(props)
-    {
-        super(props);
-
-        this.state={tileId:this.props.tileIdProp, 
-                    tileColor: this.props.tileColorProp}
-    }
-
-    changeColor = num => {
-        switch(num) {
-            case 0:
-                return 1;
-            case 1:
-                return 2;
-            case 2:
-                return 1;
-    
-            default:
-                return 0;
-        }
-    }
-    
-    render(){
-        const {tileColorProp} = this.props;
-        return(
-            <Mutation mutation={update_Tile}>
-                {(updateTile, { data }) => (
-                    <button 
-                        className="tile"
-                        onClick={ () => {                            
-                            const temp = this.changeColor(this.state.tileColor);
-                            this.setState({tileColor:temp});
-                            updateTile({ variables: { tileId : this.state.tileId,
-                                                        tileColor: temp}}).then(({data}) => console.log('ret', data)
-                                                        );
-                            
-                        } }
-
-                        style={{background: getColor(tileColorProp)}}>
-                    </button>
-                )}
-            </Mutation>
-    )}
+    return(
+            <button 
+                className="tile"
+                onClick={ () => { updateTile(tileIdProp, tileColorProp) } }
+                style={{background: getColor(tileColorProp)}}>
+            </button>
+        )
 }
 
 export default TileButton;
--- src/View.js
+++ src/View.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import { graphql, compose} from "react-apollo";
 import  TileButton  from "./TileButton";
 import  "./View.css";
 
-const getTiles = gql`
-    query {
-        getTiles {
+const GET_TILES_QUERY = gql`
+    query GetTiles($id: Int){
+        getTiles(id: $id) {
             id
             tileColor
         }
     }
 `;
 
-const onChangeTile = gql`
+const ON_CHANGE_TILE_QUERY = gql`
     subscription {
         onChangeTile {
             id
@@ -22,42 +22,94 @@ const onChangeTile = gql`
     }
 `;
 
-let unsubscribe = null;
+const UPDATE_TILE = gql`
+    mutation UpdateTile($tileId: Int!, $tileColor: Int!) {
+        updateTile(tileId: $tileId, tileColor: $tileColor) {
+            val
+        }
+    }
+`;
 
-const View = () => (
-<Query query={getTiles}>
-    {({ loading, data, subscribeToMore}) => {
-        if (loading) {
-            return null;
-        }
-        if (!unsubscribe) {
-            unsubscribe = subscribeToMore({
-                document: onChangeTile,
-                updateQuery: (prev, { subscriptionData}) => {
-                    if(!subscriptionData.data) return prev;
-                    const { onChangeTile } = subscriptionData.data;
-                    prev.getTiles[onChangeTile.id].tileColor = onChangeTile.tileColor;
-                    return {
-                        ...prev
-                    };
+const changeColor = num => {
+    switch(num) {
+        case 0:
+            return 1;
+        case 1:
+            return 2;
+        case 2:
+            return 1;
+
+        default:
+            return 0;
+    }
+}
+
+
+class View extends Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            tiles : []
+        };        
+    }
+
+    componentWillMount() {
+        this.props.data.subscribeToMore({
+            document: ON_CHANGE_TILE_QUERY,
+            updateQuery: (prev, { subscriptionData }) => {
+                if(!subscriptionData.data) return prev;
+                const { onChangeTile } = subscriptionData.data;
+                return {
+                    ...prev
                 }
-            });
+            }
+        });
+    }
+
+    updateTile = (tileId, tileColor) => {
+        this.props.updateTile(
+            {variables: { tileId: tileId ,tileColor: changeColor(tileColor)}});
+    }
+
+    componentWillReceiveProps(nextprops) {
+        this.setState({tiles: nextprops.data.getTiles});
+    }
+
+    genButtons = () => {     
+        let temp = [];
+            for (let index = 0; index < this.state.tiles.length; index++) {
+                temp.push(
+                    <TileButton tileIdProp={this.state.tiles[index].id} 
+                    tileColorProp={this.state.tiles[index].tileColor} 
+                    updateTile={ this.updateTile}/>   
+                );
+            }       
+        return temp;
+    }
+
+    render() {
+        const {data: {loading}} = this.props;
+
+        if(loading) {
+            return (
+                <div className = "tile-container">
+                    Loading~~
+                </div>
+            )
         }
+
         return (
-            <div className="tile-container">
-                {
-                    data.getTiles.map(tile => 
-                    (
-                        <span>
-                            <TileButton tileIdProp={tile.id} tileColorProp={tile.tileColor}/>
-                            {((tile.id+1)%4 === 0)?<br></br>:""}
-                        </span>
-                    )
-                )}
+            <div className = "tile-container">
+                {this.genButtons()}
             </div>
         );
-    }}
-</Query>
-);
+    }
 
-export default View;
+};
+
+const withTileData = graphql(GET_TILES_QUERY);
+const MutateData = graphql(UPDATE_TILE, {name: "updateTile"});
+
+export default compose(withTileData, MutateData)(View);
