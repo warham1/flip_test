113610e52e82c21ecf7bdf94e5449b251621ae0e
warham1 <yellow9697@hanyang.ac.kr>
2019-07-11 05:23:37 +0900

    github pages test

---
+++ docs/precache-manifest.94b768a683494fe43b87d8337e65c2cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd743877726b0b33dea854df5a1f3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91034ea85b4152383169",
+    "url": "/static/css/main.fb1b4944.chunk.css"
+  },
+  {
+    "revision": "d821e24977b8e61ced91",
+    "url": "/static/js/2.9609fc57.chunk.js"
+  },
+  {
+    "revision": "91034ea85b4152383169",
+    "url": "/static/js/main.086e1780.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
